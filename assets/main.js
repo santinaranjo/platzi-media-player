16b7e39e3a5f8508bf63e84ec84ba359b90c70a4
santinaranjo <marketingsanti@gmail.com>
2022-01-25 20:51:43 +0100

    Codigo modularizado

--- assets/main.js
+++ assets/main.js
@@ -1,25 +1,7 @@
+import MediaPlayer from "./MediaPlayer";
+
 const video = document.querySelector("video");
 const button = document.querySelector("button");
-
-function MediaPlayer(config) {
-    this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function () {
-    this.media.play();
-}
-
-MediaPlayer.prototype.pause = function () {
-    this.media.pause();
-}
-
-MediaPlayer.prototype.togglePlay = function () {
-    if (this.media.paused) {
-        this.play();
-    } else {
-        this.pause();
-    }
-}
 
 const player = new MediaPlayer({ el: video });
 
